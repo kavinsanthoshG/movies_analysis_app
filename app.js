04e7b2a392804e1ef20ed7d6cb77af4b765c20c4
Kavin Santhosh <104335497+kavinsanthoshG@users.noreply.github.com>
2025-02-28 00:02:07 +0530

    Update app.js 3000

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ app.get('/movies/:id', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
