bb63ee5164aad1c7263def1a6e382f498d5d6b49
Kavin Santhosh <104335497+kavinsanthoshG@users.noreply.github.com>
2025-02-27 22:54:07 +0530

    done with dotenv

--- dbConfig.js
+++ dbConfig.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+console.log(process.env)
 const config = {
-    user: 'kavin',
-    password: 'KQ)#5Bz$W2-gDFX',
-    server: 'sskavin123.database.windows.net',
-    database: 'moviedb',
+    user: process.env.AZUREUSERNAME,
+    password: process.env.AZURESQLDBPASSWORD,
+    server: process.env.AZURESERVER,
+    database: process.env.AZUREDB,
     options: {
         encrypt: true, // Use encryption
         enableArithAbort: true
